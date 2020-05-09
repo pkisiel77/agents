fa28e2f64ba8d62841771645b06bfaf9922e7b80
profeosoft <profeosoft@gmail.com>
2020-05-09 21:00:23 +0200

    First commit

---
+++ KCetnarowicz/Agent.cs
@@ -0,0 +1,35 @@
+﻿namespace KCetnarowicz
+{
+    public class AgentModule
+    {
+        public int param;
+        public string name;
+        public string shortName;
+        
+        protected int minParam = 0;
+        protected int maxParam = 100;
+    }
+    public class ReasoningAbility : AgentModule
+    {
+        public ReasoningAbility()
+        {
+            name = "ReasoningAbility";
+            shortName = "RA";
+            minParam = 0;
+            maxParam = 100;
+        }
+    }
+
+    public class Objectives : AgentModule
+    {
+        public Objectives()
+        {
+            name = "Objectives";
+            shortName = "OB";
+            minParam = 0;
+            maxParam = 100;
+        }
+    }
+
+
+}
